6a7ab33b5b06307d4042077eb4c4a3f20983cf35
Alli Edwards <edwardsallistair15@gmail.com>
2023-09-05 16:08:40 +0100

    Removing print statements, input cleaning

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ reldf = df.drop(['PassengerId','Survived','Name','SibSp',
 
 # fill NaN with 0
 cleandf = reldf.fillna(0)
-print(cleandf.head(10))
 target = df['Survived']
 
 # label encode sex catergory into 0/1
 le_Sex = LabelEncoder()
 cleandf['Sex'] = le_Sex.fit_transform(cleandf['Sex'])
-print(cleandf.head(10))
 
 # training
 model = tree.DecisionTreeClassifier()
 model.fit(cleandf, target)
 # class, female/male, age, fare
-inputclass = input("Class: ")
-inputgender = input("Gender: ")
+inputclass = input("Class (1st, 2nd, 3rd): ")[0]
+inputgender = input("Gender (F/M): ")
+inputgenderadj = 0 if inputgender == "F" else 1
 inputage = input("Age: ")
-inputfare = input("Fare: ")
-if model.predict([[inputclass,inputgender,inputage,inputfare]]) == [0]:
+inputfare = input("Fare (in £): ")
+
+if model.predict([[inputclass,inputgenderadj,inputage,inputfare]]) == [0]:
     print("\n\nPassenger doesn't survive.\n\n")
 else:
     print("\n\nPassenger survives.\n\n")
